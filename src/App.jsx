af003e0dd96dff7f1ffd995d6f7295941385ad9c
gitankitmishra <ankitmishra.work005@gmail.com>
2024-03-30 19:24:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css"
+import Nav from "./components/Nav";
+import HeroSection from "./components/Hero";
+const App = () => {
+  return (
+    <div>
+      <Nav/>
+      <HeroSection/>
+    </div>
+  );
+};
+
+export default App;
